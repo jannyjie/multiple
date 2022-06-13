7ded605e38e401667b2d8646851ac1413b40c409
unknown <yuag1331@gmail.com>
2022-06-14 00:06:12 +0800

    初始

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,45 @@
-const { defineConfig } = require('@vue/cli-service');
+/*eslint semi: "error"*/
+const path = require('path')
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+const PORT = 8550
+const DEV_HOST = 'vmep.com'
+
+/**
+ * https://cli.vuejs.org/config/#pages
+ */
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/index/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Index Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    manage: {
+      entry: 'src/manage/main.js',
+      template: 'public/index.html',
+      filename: 'manage/index.html',
+      title: 'Manage Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'manage']
+    },
+    dashboard: {
+      entry: 'src/dashboard/main.js',
+      template: 'public/index.html',
+      filename: 'dashboard/index.html',
+      title: 'Dashboard Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'dashboard']
+    }
+  },
+
+  devServer: {
+    port: PORT,
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/manage\/?.*/, to: path.posix.join('/', 'manage/index.html') },
+        { from: /./, to: path.posix.join('/', 'index.html') }
+      ]
+    },
+    allowedHosts: [DEV_HOST]
+  }
+}
